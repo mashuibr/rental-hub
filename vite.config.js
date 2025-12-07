033e17d80e7bde7faa87e38389014e02666873f0
Mashu <mashuibr@gmail.com>
2025-12-07 12:06:02 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '',
+  base: '/jithinrenatl/',
   plugins: [react(),tailwindcss()],
   server: {
-    open: ''
+    open: '/jithinrenatl/'
   }
 })
